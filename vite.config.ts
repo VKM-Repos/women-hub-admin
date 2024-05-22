ae011cd6b0eabb2c1f59feebebaf780068b347af
jawnchuks <jawnchuks@gmail.com>
2024-05-22 16:46:31 +0100

    installed shadcn and configured absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteTsconfigPaths()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
