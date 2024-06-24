eeb0f45ad459ac27b95dc777072caa6f9ac80312
salscodes <salissadiq@gmail.com>
2024-06-24 09:59:10 +0100

    Transform different user role creation page

--- src/pages/main/Users/CreateUser.tsx
+++ src/pages/main/Users/CreateUser.tsx
@@ -1,3 +1,20 @@
+import Tag from "@/components/dashboard/Tag";
+import Back from "@/components/shared/backButton/Back";
+
+import UserDetailsForm from "./components/UserDetailsForm";
+import SubmitButton from "./components/SubmitButton";
+
 export default function CreateUser() {
-  return <div>CreateUser</div>;
+  return (
+    <div>
+      <div className="bg-white p-5 rounded-md w-[80%] mx-auto">
+        <div className="flex items-center justify-between">
+          <Tag title="User Information" />
+          <Back />
+        </div>
+        <UserDetailsForm />
+      </div>
+      <SubmitButton />
+    </div>
+  );
 }
