ecec624004134714aba539be3f0699804f00deee
salscodes <salissadiq@gmail.com>
2024-06-24 09:52:17 +0100

    Remove top margin to allow flexible margin

--- src/components/dashboard/Tag.tsx
+++ src/components/dashboard/Tag.tsx
@@ -4,7 +4,7 @@ type TagProps = {
 };
 export default function Tag({ title }: TagProps) {
   return (
-    <div className="flex items-center gap-5 mt-7">
+    <div className="flex items-center gap-5">
       <span className={`h-10 w-5 bg-tagBg rounded-md`} />
       <h2 className="text-xl font-semibold">{title}</h2>
     </div>
