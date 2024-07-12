9cb547c0020572eb636f5858b2c42994a45eaeb7
salscodes <salissadiq@gmail.com>
2024-07-12 12:26:45 +0100

    Implement app persistent store for storing user details

---
+++ src/lib/store/app.store.ts
@@ -0,0 +1,56 @@
+import { User } from "lucide-react";
+import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
+
+type User = {
+  userId: number;
+  email: string;
+  name: string;
+  organization: string;
+  refreshToken: string;
+  role: string;
+  token: string;
+};
+interface UserState {
+  user: User;
+  loggedIn: boolean | string;
+  login: (user: User, loggedIn: boolean) => void;
+  logout: () => void;
+}
+
+const useAppStore = create<UserState>()(
+  devtools(
+    persist(
+      (set) => ({
+        user: {
+          userId: 0,
+          email: "",
+          name: "",
+          organization: "",
+          refreshToken: "",
+          role: "",
+          token: "",
+        },
+        loggedIn: false,
+        login: (user: User, loggedIn: boolean) =>
+          set(() => ({ user, loggedIn })),
+        logout: () =>
+          set(() => ({
+            user: {
+              userId: 0,
+              email: "",
+              name: "",
+              organization: "",
+              refreshToken: "",
+              role: "",
+              token: "",
+            },
+            loggedIn: false,
+          })),
+      }),
+      { name: "useAppStore" }
+    )
+  )
+);
+
+export default useAppStore;
