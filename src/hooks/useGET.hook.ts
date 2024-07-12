35e4d3f91e26b1a74c092431283e9b14f6beeb02
salscodes <salissadiq@gmail.com>
2024-07-12 12:28:19 +0100

    Implement generic GET for handling get request

---
+++ src/hooks/useGET.hook.ts
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import { authApi, publicApi } from "@/lib/config/axiosInstance";
+
+
+export const useGET = ({ url, queryKey, withAuth = false, enabled }: { url: string, queryKey: any[], withAuth: boolean, enabled: boolean }) => {
+  const fetch = async () => {
+    const axiosInstance = withAuth ? authApi : publicApi;
+    const response = await axiosInstance.get(url);
+    return response?.data;
+  };
+
+  const {
+    data,
+    isFetching,
+    isLoading,
+    isPending,
+    isError,
+    isSuccess,
+    refetch,
+    isRefetching,
+    isLoadingError,
+    isRefetchError,
+  } = useQuery({ queryKey: queryKey, queryFn: fetch });
+  return {
+    data,
+    isFetching,
+    isLoading,
+    isPending,
+    isError,
+    isSuccess,
+    refetch,
+    isRefetching,
+    isLoadingError,
+    isRefetchError,
+  };
+};
+
