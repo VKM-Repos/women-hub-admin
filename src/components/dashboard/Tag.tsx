9e168b8933f1351629193f3b1aa8ad14d75bb4df
salscodes <salissadiq@gmail.com>
2024-08-12 12:28:12 +0100

    Refactor Tag component to accept color as a props

--- src/components/dashboard/Tag.tsx
+++ src/components/dashboard/Tag.tsx
@@ -1,11 +1,12 @@
+import { cn } from "@/lib/utils";
 type TagProps = {
   title: string;
-  /*color: string;*/
+  color: string;
 };
-export default function Tag({ title }: TagProps) {
+export default function Tag({ title, color }: TagProps) {
   return (
     <div className="flex items-center gap-5">
-      <span className={`h-10 w-5 bg-tagBgOrg rounded-md`} />
+      <span className={cn("h-10 w-5  rounded-md", color)} />
       <h2 className="text-xl font-semibold">{title}</h2>
     </div>
   );
