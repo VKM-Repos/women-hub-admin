30b0a767c7e3e4f291bf25beab8e78679e48de4f
salscodes <salissadiq@gmail.com>
2024-08-12 12:29:20 +0100

    Create a generic loading component

---
+++ src/components/shared/Loading.tsx
@@ -0,0 +1,15 @@
+import loadingLogo from "@/assets/loading-logo.svg";
+
+export default function Loading() {
+  return (
+    <div className="fixed inset-0 backdrop-brightness-50 flex items-center justify-center z-[2500]">
+      <div className="relative flex justify-center items-center">
+        <img
+          src={loadingLogo}
+          alt="Loading logo"
+          className="rounded-full h-28 w-28 animate-spin  ease-in-out"
+        />
+      </div>
+    </div>
+  );
+}
