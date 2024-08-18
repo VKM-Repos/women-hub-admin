5a35cf8031071a414e89f824105a6b162622b40f
jawnchuks <jawnchuks@gmail.com>
2024-08-18 17:05:29 +0100

    created a custom submit button, don't know if i'll use it

---
+++ src/components/shared/submitButton.tsx
@@ -0,0 +1,35 @@
+
+import { Button } from "../ui/button";
+
+interface ButtonProps {
+  isLoading: boolean;
+  className?: string;
+  children: React.ReactNode;
+}
+
+const SubmitButton = ({ isLoading, className, children }: ButtonProps) => {
+  return (
+    <Button
+      type="submit"
+      disabled={isLoading}
+      className={className ?? "w-full"}
+    >
+      {isLoading ? (
+        <div className="flex items-center gap-4">
+          {/* <img
+            src="/assets/icons/loader.svg"
+            alt="loader"
+            width={24}
+            height={24}
+            className="animate-spin"
+          /> */}
+          <span>Please wait...</span>
+        </div>
+      ) : (
+        children
+      )}
+    </Button>
+  );
+};
+
+export default SubmitButton;
