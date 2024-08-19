46390b8fbe5ecce7a465f6815d20e7ee97ab24e6
Laouali Bachir <laoualibachir2000@gmail.com>
2024-08-19 15:06:01 +0100

    edit guide header

---
+++ src/pages/main/Support/mockupData/guideCard-mockup-data.ts
@@ -0,0 +1,30 @@
+import GettingStart from "@/assets/images/Getting started.png";
+import FAQ from "@/assets/images/FAQ.png";
+import HELPLINES from "@/assets/images/HELPLINES.png";
+
+export const guideCardData = [
+  {
+    id: "1",
+    title: "A Guide to  Women Hub",
+    coverImageUrl: GettingStart,
+    description:
+      "Learn about our community guidelines and policies to ensure a safe and welcoming environment for everyone.",
+    info: "View guidelines",
+  },
+  {
+    id: "2",
+    title: "FAQs",
+    coverImageUrl: FAQ,
+    description:
+      "Find answers to common questions and learn more about how to use Women Hub effectively.",
+    info: `View FAQ’s`,
+  },
+  {
+    id: "3",
+    title: "Helplines",
+    coverImageUrl: HELPLINES,
+    description:
+      "Helpline provides access to various emergency contacts dedicated to supporting women in times of crisis.",
+    info: "View Helplines",
+  },
+];
