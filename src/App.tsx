bb629b6824f98d4fcbb887a607e54e5a410df7dd
Laouali Bachir <laoualibachir2000@gmail.com>
2024-08-30 16:10:00 +0100

    view a ticket

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Toaster toastOptions={toastConfig} />
-      <Routes isAuthorized={true} />
+      <Routes isAuthorized={loggedIn} />
     </QueryClientProvider>
   );
 }
