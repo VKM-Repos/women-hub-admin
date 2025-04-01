929595d4a7986980bf0e1de58d2c4ddbb4e41a13
Susu-spec <suleimansuwaibat@gmail.com>
2025-04-01 17:16:01 +0100

    add description to homepage linechart

--- src/pages/main/Home/Index.tsx
+++ src/pages/main/Home/Index.tsx
@@ -8,9 +8,13 @@ import RecentPostCard from "@/components/dashboard/RecentPostCard";
 import useAppStore from "@/lib/store/app.store";
 import { useGET } from "@/hooks/useGET.hook";
 import Loading from "@/components/shared/Loading";
+import { useState } from "react";
 
 export default function Home() {
+  const [period, setPeriod ] = useState('WEEKLY');
+
   const { user } = useAppStore();
+
   const { data: usersStats, isPending } = useGET({
     url: "admin/stats/user-statistics",
     queryKey: ["USER_STATISTICS"],
@@ -20,7 +24,11 @@ export default function Home() {
     url: "admin/stats/post-metrics",
     queryKey: ["POST_METRICS_STATISTICS"],
   });
-  console.log(postMetrics, "??????");
+
+  const handleTabsChange = (period: string) => {
+    setPeriod(period);
+  }
+
 
   return (
     <>
@@ -45,10 +53,28 @@ export default function Home() {
                 </div>
               </div>
               <div className="flex gap-5 mt-10 mb-10">
-                <div className="bg-white rounded-md drop-shadow-lg w-[70%]">
-                  <LineChart />
+                <div className="bg-white rounded-md drop-shadow-lg w-[70%] max-h-fit">
+                  <div className="p-4 pb-0 flex justify-between items-center">
+                    <p className="font-bold">User Engagement</p>
+                      <div className="flex items-center justify-center gap-4">
+                        <p className="flex items-center justify-center gap-1">
+                          <span className="border border-secondary bg-secondary w-5 h-5 rounded-full"></span>
+                          <span className="text-textPrimary font-semibold text-xs">Discussion</span>
+                        </p>
+                        <p className="flex items-center justify-center gap-1">
+                          <span className="border border-[#65B891] bg-[#65B891] w-5 h-5 rounded-full"></span>
+                          <span className="text-textPrimary font-semibold text-xs">Organization Likes</span>
+                        </p>
+                      </div>
+                      <div className="bg-[#F4F5F9] rounded-md max-w-[12.5rem-] py-[.375rem] px-1.5">
+                      <button className={`${period === 'WEEKLY' ? `bg-white text-black` : 'text-black/30'} text-base py-[0.563rem] px-4 rounded-md`} onClick={() => {handleTabsChange('WEEKLY')}}>Weekly</button>
+                        <button className={`${period === 'MONTHLY' ? `bg-white text-black` : 'text-black/30'} text-base py-[0.563rem] px-4 rounded-md`} onClick={() => {handleTabsChange('MONTHLY')}}>Monthly</button>
+                      </div>
+                    </div>
+
+                  <LineChart period={period} />
                 </div>
-                <div className="bg-white drop-shadow-lg rounded-md w-[30%] flex flex-col px-4 py-4">
+                <div className="bg-white drop-shadow-lg rounded-md w-[30%] flex flex-col px-4 py-4 justify-between">
                   <h2 className="text-base font-bold">
                     Recent Support Tickets
                   </h2>
